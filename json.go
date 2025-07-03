9e5fd06422556081801d50f3a9a9af08c8fd230f
Daniel Hayhurst <danhursty@gmail.com>
2025-07-03 01:24:20 -0700

    fixed sec errors

--- json.go
+++ json.go
@@ -30,5 +30,10 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	_, err = w.Write(dat)
+	if err != nil {
+		log.Printf("Error writting data to request %s", err)
+		w.WriteHeader(500)
+		return
+	}
 }
